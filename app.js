b024eaed0276b7fc77adbfc514441e2417373cdf
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-09-12 13:32:18 +0530

    cors headers added

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const bodyParser=require('body-parser')
+const bodyParser = require('body-parser')
 
 const productsRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE')
+        return res.status(200).json({})
+    }
+    next()
+})
 
 app.use('/products', productsRoutes)
 app.use('/orders', orderRoutes)
