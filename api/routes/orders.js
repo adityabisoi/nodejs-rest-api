fd0e7647cceb42658c128bc42f5dc622e180fa0e
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-09-14 19:35:29 +0530

    order POST

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -11,10 +11,23 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    }
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        productId: req.body.productId
+    })
+    order.save()
+        .exec()
+        .then((data) => {
+            console.log(data)
+            res.status(201).json(data)
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
     res.status(201).json({
         message: '/orders POST',
         order: order
