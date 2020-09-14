cc30e91987e9d084fc936131c6af58619d4452ec
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-09-14 09:02:04 +0530

    post methods

--- api/routes/products.js
+++ api/routes/products.js
@@ -15,6 +15,12 @@ router.post('/', (req, res, next) => {
         name: req.body.name,
         price: req.body.price
     })
+    product.save().then((result) => {
+            console.log(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
     res.status(201).json({
         message: '/products POST',
         createdProduct: product
